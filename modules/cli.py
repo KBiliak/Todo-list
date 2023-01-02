b05746ba4892401229c80b58aee8ddbaaf4dfba1
Kate Biliak <kate.bilyak@gmail.com>
2023-01-02 13:26:59 -0500

    Create basic GUI without interactions, day 16

--- modules/cli.py
+++ modules/cli.py
@@ -1,14 +1,8 @@
-def get_todos(filepath = "todos.txt"):
-    """Read a text file and return the list of to-do items"""
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+from modules.functions import get_todos, write_todos
+import time
 
-def write_todos(todos_arg, filepath = "todos.txt"):
-
-    with open(filepath, "w") as file:
-        file.writelines(todos_arg)
-        # return todos_arg
+now = time.strftime("%D, %T")
+print("It is", now)
 
 while True:
     user_action = input("Choose the action: add, show, edit, exit or complete: ")
@@ -21,7 +15,7 @@ while True:
 
         todos.append(todo + "\n")
 
-        write_todos(filepath = "todos.txt", todos_arg = todos)
+        write_todos(filepath ="todos.txt", todos_arg = todos)
 
     elif user_action.startswith("show"):
 
